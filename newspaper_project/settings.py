9ccb1bb7b142276432b7e6bd1780a0cca9f3c589
FrankRight <frankbosire2017@gmail.com>
2020-01-03 11:56:08 +0300

    New Apdate Jan 3.2

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*xb1l8ekgee*d3)&7cbi&=i)iksb2&vzti^!4o#on0p%s#p6r4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
